55cb74429f8c46334ea40b3d2a473613c2e1f615
TacoGlz <axeldlg1305@gmail.com>
2026-04-16 20:55:42 -0600

    ports

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  
+  app.enableCors(); // <--- Importante para que tu React pueda conectarse
+
+  // Render usa la variable de entorno PORT. Si no existe, usa 3000 (local).
+  const port = process.env.PORT || 3000;
+  
+  await app.listen(port, '0.0.0.0'); // Escuchar en todas las interfaces
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
