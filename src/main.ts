d5b5e69d89aa7361e67a350147b1d7b29bb27851
TacoGlz <axeldlg1305@gmail.com>
2026-04-16 21:22:22 -0600

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors(); // <--- Importante para que tu React pueda conectarse
-
+app.enableCors({
+  origin: 'https://stripe-api-gewm.onrender.com', // La URL que te dio Netlify
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type,Authorization',
+});
   // Render usa la variable de entorno PORT. Si no existe, usa 3000 (local).
   const port = process.env.PORT || 3000;
   
